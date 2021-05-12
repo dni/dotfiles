b00736bfe357ed409fbaf4e2a8252545e655b067
dni <dni.khr@gmail.com>
2021-05-12 23:15:05 +0200

    mumble update

--- setup/server/install-mumble.sh
+++ setup/server/install-mumble.sh
@@ -4,3 +4,19 @@ apt install -y mumble-server
 ufw allow 64738/tcp
 ufw allow 64738/udp
 dpkg-reconfigure mumble-server
+cat <<EOF > /etc/mumble-server.ini
+database=/var/lib/mumble-server/mumble-server.sqlite
+icesecretwrite=
+logfile=/var/log/mumble-server/mumble-server.log
+pidfile=/var/run/mumble-server/mumble-server.pid
+welcometext="<br />Hostinghelden Mumble<br />Enjoy your stay!<br />"
+port=64738
+serverpassword=%CHANGE PW%
+bandwidth=72000
+users=20
+uname=mumble-server
+[Ice]
+Ice.Warn.UnknownProperties=1
+Ice.MessageSizeMax=65536
+EOF
+vim /etc/mumble-server.ini
