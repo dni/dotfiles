523f13ea3ea9e875b1003d325d14dde838121d9f
dni <dni.khr@gmail.com>
2021-05-12 11:13:38 +0200

    update wireguard install script

--- setup/server/install-wireguard.sh
+++ setup/server/install-wireguard.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 endpoint=wireguard.hostinghelden.at:61951
 
-sudo apt install wireguard
-sudo ufw allow 61951/udp
+apt-get update
+apt-get upgrade
+apt install wireguard
+ufw allow 61951/udp
 echo "Uncomment the following line. net.ipv4.ip_forward=1"
 echo "ok? (y)"
-read
-sudo vim /etc/sysctl.conf
-sudo sysctl -p
+read -n 1
+vim /etc/sysctl.conf
+sysctl -p
 
 wg_add_key() {
   echo "$1: private and public key pairs creation"
-  wg genkey | sudo tee /etc/wireguard/$1_private.key | wg pubkey | sudo tee /etc/wireguard/$1_public.key
-  sudo chmod 600 /etc/wireguard/$1_private.key
+  wg genkey | tee /etc/wireguard/$1_private.key | wg pubkey | tee /etc/wireguard/$1_public.key
+  chmod 600 /etc/wireguard/$1_private.key
 }
 
 wg_add_key server
@@ -59,3 +61,7 @@ add_client client4
 
 tar -czvf clients.tar.gz /etc/wireguard/client*
 echo "written ./clients.tar.gz"
+
+echo "run following commands to start wireguard and enable it on bootup"
+echo "systemctl start wg-quick@wg0"
+echo "systemctl enable wg-quick@wg0"
