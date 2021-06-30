87fc0252513c2b6e1848a23d2557ab0e9626e1fd
dni <dni.khr@gmail.com>
2021-06-30 09:42:39 +0200

    make audio scripts more elegant

--- scripts/audio.sh
+++ scripts/audio.sh
@@ -4,27 +4,25 @@ get_sinks(){
   echo $(pamixer --list-sink | grep "^[0-9]" | awk '{print $1}')
 }
 
-volume_down() {
-  get_sinks | while read -r x; do
-    pamixer --sink "$x" -d 5
+volume_command(){
+  for x in $(get_sinks); do
+    pamixer --sink "$x" "$@"
   done
 }
 
+volume_down() {
+  volume_command -d 5
+}
+
 volume_up() {
-  get_sinks | while read -r x; do
-    pamixer --sink "$x" -i 5
-  done
+  volume_command -i 5
 }
 
 volume_set() {
   [ -n "$1" ] || exit
-  get_sinks | while read -r x; do
-    pamixer --sink "$x" --set-volume "$1"
-  done
+  volume_command --set-volume "$1"
 }
 
 mute() {
-  get_sinks | while read -r x; do
-    pamixer --sink "$x" -t
-  done
+  volume_command -t
 }
